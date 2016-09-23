10fa03fa89c1c0b5777e4304f4bef2e0a9f8bcf0
Nate Dunn <hello@natedunn.net>
2016-09-22 18:33:13 -0700

    Update gulp file with new structure

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ var reload = browserSync.reload;
 gulp.task('browser-sync', function() {
   browserSync({
     server: {
-      baseDir: "./test"
+      baseDir: "./"
     }
   });
 });
@@ -53,7 +53,7 @@ gulp.task('bs-reload', function() { browserSync.reload(); });
 
 gulp.task('watch', ['browser-sync'], function() {
   // HTML Files
-  gulp.watch('./test/**/*.html'), ['bs-reload'];
+  gulp.watch('./**/*.html'), ['bs-reload'];
   // Project Styles
   gulp.watch('./**/*.scss', ['build'])
 });
@@ -64,7 +64,7 @@ gulp.task('watch', ['browser-sync'], function() {
 
 // Dev Styles
 gulp.task('styles-dev', function() {
-  sass('test/styles/test.scss')
+  sass('src/test.scss')
     .pipe(plumber({ errorHandler: onError }))
     .pipe(postcss([
       autoprefixer({browsers: ['last 2 version']}),
@@ -77,7 +77,6 @@ gulp.task('styles-dev', function() {
     ]))
     .pipe(sourcemaps.init())
     .pipe(concat('final.css'))
-    .pipe(gulp.dest('./test/styles'))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.reload({ stream: true }))
     .pipe(notify({ message: 'DEVELOPMENT STYLES task complete'}))
@@ -85,7 +84,7 @@ gulp.task('styles-dev', function() {
 
 // Styles Production
 gulp.task('styles-prod', function() {
-  return sass('test/styles/test.scss', { style: 'nested' })
+  return sass('src/test.scss', { style: 'nested' })
     .pipe(plumber({ errorHandler: onError }))
     .pipe(postcss([
       autoprefixer({browsers: ['last 2 version']}),
@@ -100,28 +99,27 @@ gulp.task('styles-prod', function() {
       })
     ]))
     .pipe(concat('final.min.css'))
-    .pipe(gulp.dest('./test/styles'))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.reload({ stream: true }))
     .pipe(notify({ message: 'PRODUCTION STYLES task complete' }));
 });
 
-// IE Styles
-gulp.task('styles-ie', function() {
-  return sass(['styles/manifest-ie.scss'], { style: 'nested' })
-    .pipe(plumber({ errorHandler: onError }))
-    .pipe(postcss([autoprefixer({browsers: [
-      'last 2 version',
-      'ie 7',
-      'ie 8',
-      'ie 9'
-    ]})]))
-    .pipe(concat('ie.main.css'))
-    .pipe(gulp.dest('./test/styles'))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.reload({ stream: true }))
-    .pipe(notify({ message: 'IE STYLES task complete' }));
-});
+// // IE Styles
+// gulp.task('styles-ie', function() {
+//   return sass(['styles/manifest-ie.scss'], { style: 'nested' })
+//     .pipe(plumber({ errorHandler: onError }))
+//     .pipe(postcss([autoprefixer({browsers: [
+//       'last 2 version',
+//       'ie 7',
+//       'ie 8',
+//       'ie 9'
+//     ]})]))
+//     .pipe(concat('ie.main.css'))
+//     .pipe(gulp.dest('./test/styles'))
+//     .pipe(gulp.dest('./css'))
+//     .pipe(browserSync.reload({ stream: true }))
+//     .pipe(notify({ message: 'IE STYLES task complete' }));
+// });
 
 // ======================
 // IMAGES
@@ -142,7 +140,6 @@ gulp.task('images', ['clean:images'], function() {
 
 // Clean Styles
 gulp.task('clean:styles', function () {
-  return del('./test/styles/**/*.css');
   return del('./css/**/*.css');
 });
 
