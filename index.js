aaa8f04876b091b090c1ec7279c02b3ab21a3d70
Nate Dunn <hello@natedunn.net>
2019-01-24 14:33:47 -0600

    Attempt to add npx script

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+function execute(command) {
+  const exec = require("child_process").exec;
+
+  exec(command, (err, stdout, stderr) => {
+    process.stdout.write(stdout);
+  });
+}
+
+execute(
+  'npm i -D tailwindcss frontiersusa/press-css#v5.0.0 && npm i -g copyfiles && copyfiles "./node_modules/press-css/tailwind.js" "./" -f  -E -V'
+);
