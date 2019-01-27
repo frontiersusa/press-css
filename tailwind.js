4d302d7cd1502b3e0280031f5171774eb9f3d03c
Nate Dunn <hello@natedunn.net>
2019-01-26 22:40:03 -0600

    Update fonts and add test buttons

--- tailwind.js
+++ tailwind.js
@@ -66,8 +66,18 @@ module.exports = {
     xl: "1200px"
   },
   fonts: {
-    sans:
-      '"Lato", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    sans: [
+      "Lato",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "Segoe UI",
+      "Helvetica",
+      "Arial",
+      "sans-serif",
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol"
+    ],
     serif: [
       "PT Serif",
       "Constantia",
@@ -91,15 +101,17 @@ module.exports = {
     ]
   },
   textSizes: {
-    xs: ".75rem", // 12px
+    xs: ".75rem",
     sm: ".875rem", // 14px
     base: "1rem", // 16px
     lg: "1.125rem", // 18px
     xl: "1.25rem", // 20px
     "2xl": "1.5rem", // 24px
-    "3xl": "1.875rem", // 30px
-    "4xl": "2.25rem", // 36px
-    "5xl": "3rem" // 48px
+    "3xl": "1.75rem", // 28px
+    "4xl": "2rem", // 28px
+    "5xl": "2.25rem", // 36px
+    "6xl": "3rem", // 48px
+    "7xl": "4rem" // 64px
   },
   fontWeights: {
     hairline: 100,
@@ -359,7 +371,32 @@ module.exports = {
       center: true,
       padding: "1rem"
     }),
-    require("tailwindcss-object-fit")(["responsive"])
+    require("tailwindcss-object-fit")(["responsive"]),
+    function({ addComponents }) {
+      const buttons = {
+        ".btn": {
+          padding: ".5rem 1rem",
+          borderRadius: ".25rem",
+          fontWeight: "600"
+        },
+        ".btn-blue": {
+          backgroundColor: "#3490dc",
+          color: "#fff",
+          "&:hover": {
+            backgroundColor: "#2779bd"
+          }
+        },
+        ".btn-red": {
+          backgroundColor: "#e3342f",
+          color: "#fff",
+          "&:hover": {
+            backgroundColor: "#cc1f1a"
+          }
+        }
+      };
+
+      addComponents(buttons);
+    }
   ],
   options: {
     prefix: "",
