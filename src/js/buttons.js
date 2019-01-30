e0ba5da50b0f3de09d0dbf7d27eec2f1e5e477f2
Nate Dunn <hello@natedunn.net>
2019-01-29 20:39:05 -0600

    Finish adding buttons

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,13 +1,27 @@
 const _ = require("lodash");
 
-module.exports = function({ buttons, variants }) {
-  return function({ addUtilities, e }) {
-    const utilities = _.map(buttons, ([color, hover], name) => ({
-      [`.button-${e(name)}`]: {
-        "background-color": `${color}`
-      }
-    }));
+module.exports = function({ buttons }) {
+  return function({ addComponents, e }) {
+    const components = [
+      {
+        ".button": {
+          "@apply bg-primary no-underline text-white font-bold py-2 px-3 rounded-full": {},
+          "&:hover": {
+            "@apply bg-primary-dark": {}
+          }
+        }
+      },
+      _.map(buttons, ([normalState, hoverState], name) => ({
+        [`.button-${e(name)}`]: {
+          "background-color": `${normalState}`,
+          "@apply no-underline text-white font-bold py-2 px-3 rounded-full": {},
+          "&:hover": {
+            "background-color": `${hoverState}`
+          }
+        }
+      }))
+    ];
 
-    addUtilities(utilities, variants);
+    addComponents(components);
   };
 };
