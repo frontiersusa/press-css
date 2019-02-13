cc32e8e5ce9f60becec7d5585fd8b5e0ef3b6a75
Nate Dunn <hello@natedunn.net>
2019-02-12 19:39:51 -0600

    Break up tailwind “config” file

---
+++ plugins.js
@@ -0,0 +1,37 @@
+const tailwindContainer = require('tailwindcss/plugins/container');
+const tailwindObjectFit = require('tailwindcss-object-fit');
+const pressTypography = require('./src/utilities/typography');
+const pressForms = require('./src/utilities/forms');
+const pressLinks = require('./src/components/links');
+const pressButtons = require('./src/components/buttons');
+const pressImportant = require('./src/variants/important');
+const theme = require('./theme');
+
+module.exports = [
+  // Press Utilities
+  pressTypography(),
+  pressForms(),
+  // Press Components
+  pressLinks({
+    links: {
+      primary: [theme.colors.primary, theme.colors.secondary],
+      secondary: [theme.colors.secondary, theme.colors['secondary-light']],
+      danger: [theme.colors.danger, theme.colors['danger-dark']],
+    },
+  }),
+  pressButtons({
+    buttons: {
+      primary: [theme.colors.primary, theme.colors['primary-dark']],
+      secondary: [theme.colors.secondary, theme.colors['secondary-light']],
+      danger: [theme.colors.danger, theme.colors['danger-dark']],
+    },
+  }),
+  // Press Variants
+  pressImportant(),
+  // Press Dependencies
+  tailwindContainer({
+    center: true,
+    padding: '1rem',
+  }),
+  tailwindObjectFit(['responsive']),
+];
