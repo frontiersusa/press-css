f0117a36f6d2f0a8af5bcd5bb88dd30258616673
Nate Dunn <hello@natedunn.net>
2019-02-13 20:53:51 -0600

    Remove bold from buttons

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -5,7 +5,7 @@ module.exports = function({ buttons }) {
     const components = [
       {
         '.button': {
-          '@apply bg-primary no-underline text-white font-bold py-2 px-3 rounded-full': {},
+          '@apply bg-primary no-underline text-white py-2 px-3 rounded-full': {},
           '&:hover': {
             '@apply bg-primary-dark': {},
           },
@@ -14,7 +14,7 @@ module.exports = function({ buttons }) {
       _.map(buttons, ([normalState, hoverState], name) => ({
         [`.button-${e(name)}`]: {
           'background-color': `${normalState}`,
-          '@apply no-underline text-white font-bold py-2 px-3 rounded-full': {},
+          '@apply no-underline text-white py-2 px-3 rounded-full': {},
           '&:hover': {
             'background-color': `${hoverState}`,
           },
