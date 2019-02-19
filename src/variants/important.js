4e7571fc0a959536131915ff3bebdfcafb4e6a31
Nate Dunn <hello@natedunn.net>
2019-02-19 13:10:21 -0600

    Change important variant syntax from `!` to `important:`

--- src/variants/important.js
+++ src/variants/important.js
@@ -2,7 +2,7 @@ module.exports = function() {
   return function({ addVariant }) {
     addVariant('important', ({ container }) => {
       container.walkRules(rule => {
-        rule.selector = `.\\!${rule.selector.slice(1)}`;
+        rule.selector = `.important\\:${rule.selector.slice(1)}`;
         rule.walkDecls(decl => {
           decl.important = true;
         });
