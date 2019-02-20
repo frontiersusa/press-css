0609090e06f5b8c095c38cc8f264a25f4808826f
Nate Dunn <hello@natedunn.net>
2019-02-20 16:16:12 -0600

    Add better notices in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
+echo -e "\n\e[32mUpdating Browser List. Opening a browser window to continue."
 # Update Browser List data
 npx browserslist-ga
 git add .
 git commit -m "Update Browser List"
 
+echo -e "\n\e[32mReleasing a new version."
 # Create release for Github
 source .env
 export GITHUB_TOKEN
 release-it
 
 # Complete
-echo -e "Deploying 'docs' directory to 'gh-pages' branch."
-
+echo -e "\n\e[32mDeploying 'docs' directory to 'gh-pages' branch."
 git push origin `git subtree split --prefix docs master`:gh-pages --force
