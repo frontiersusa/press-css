741cf975ebf8e1262dc082b455c2fdd92db9c21b
Nate Dunn <hello@natedunn.net>
2019-02-25 19:20:20 -0600

    Reorganize and add complete deploy script message

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
-echo -e "\n\e[32mUpdating Browser List. Opening a browser window to continue."
 # Update Browser List data
+echo -e "\n\e[32mUpdating Browser List. Opening a browser window to continue."
 npx browserslist-ga
 git add .
 git commit -m "Update Browser List"
 
-echo -e "\n\e[32mReleasing a new version."
 # Create release for Github
+echo -e "\n\e[32mReleasing a new version."
 source .env
 export GITHUB_TOKEN
 release-it
 
-# Complete
+# Deploy to gh-pages
 echo -e "\n\e[32mDeploying 'docs' directory to 'gh-pages' branch."
 git push origin `git subtree split --prefix docs master`:gh-pages --force
+
+# Complete
+echo -e "\n\e[32mDeploy script complete."
