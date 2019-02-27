8f8ab091d721e3f26c5c2d282b4271e16f639325
Nate Dunn <hello@natedunn.net>
2019-02-27 12:58:08 -0600

    Move config directory to root of /src

--- src/plugins.js
+++ src/plugins.js
@@ -1,16 +1,14 @@
 const tailwindContainer = require('tailwindcss/plugins/container');
 const tailwindObjectFit = require('tailwindcss-object-fit');
-const pressTypography = require('../utilities/typography');
-const pressForms = require('../utilities/forms');
-const pressLinks = require('../components/links');
-const pressButtons = require('../components/buttons');
-const pressImportant = require('../variants/important');
+const pressTypography = require('./utilities/typography');
+const pressLinks = require('./components/links');
+const pressButtons = require('./components/buttons');
+const pressImportant = require('./variants/important');
 const theme = require('./theme');
 
 module.exports = [
   // Press Utilities
   pressTypography(),
-  pressForms(),
   // Press Components
   pressLinks({
     links: {
