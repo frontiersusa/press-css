26e467fc827145e278cd948cbbe367f733aff2c3
Nate Dunn <hello@natedunn.net>
2019-02-27 23:01:44 -0600

    Change link components

--- src/plugins.js
+++ src/plugins.js
@@ -12,12 +12,14 @@ module.exports = [
   // Press Components
   pressLinks({
     links: {
-      primary: [theme.colors.primary, theme.colors.secondary],
-      secondary: [theme.colors.secondary, theme.colors['secondary-light']],
-      white: [theme.colors.white, theme.colors.primary],
+      primary: [theme.colors['primary-light'], theme.colors['primary-dark']],
+      secondary: [
+        theme.colors['secondary-light'],
+        theme.colors['secondary-dark'],
+      ],
+      accent: [theme.colors['accent-light'], theme.colors['accent-dark']],
       red: [theme.colors['red-light'], theme.colors['red-dark']],
       orange: [theme.colors['orange-light'], theme.colors['orange-dark']],
-      green: [theme.colors['primary-light'], theme.colors['primary-dark']],
     },
   }),
   pressButtons({
