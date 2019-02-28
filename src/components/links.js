75d33abd8ea7bf11c833fe835e52940666af66eb
Nate Dunn <hello@natedunn.net>
2019-02-27 23:01:22 -0600

    Remove default link class

--- src/components/links.js
+++ src/components/links.js
@@ -3,14 +3,6 @@ const _ = require('lodash');
 module.exports = function({ links }) {
   return function({ addComponents, e }) {
     const components = [
-      {
-        '.link': {
-          '@apply border-b-2 border-primary no-underline color-current': {},
-          '&:hover': {
-            '@apply border-secondary': {},
-          },
-        },
-      },
       _.map(links, ([normalState, hoverState], name) => ({
         [`.link-${e(name)}`]: {
           'border-color': `${normalState}`,
@@ -22,7 +14,6 @@ module.exports = function({ links }) {
         },
       })),
     ];
-
     addComponents(components);
   };
 };
