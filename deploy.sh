44021f908d17f3c2f4fe3653c58c7e1c67ee43cd
Nate Dunn <hello@natedunn.net>
2019-03-28 12:58:57 -0600

    Use emojis instead of colors

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 # Update Browser List data
-echo "\n \e[32m Updating Browser List. Opening a browser window to continue."
+echo "\n 🌎  Updating Browser List. Opening a browser window to continue."
 npx browserslist-ga
 git add .
 git commit -m "Update Browser List"
 
 # Create release for Github
-echo "\n \e[32m Releasing a new version."
+echo "\n 🚀  Releasing a new version."
 source .env
 export GITHUB_TOKEN
 release-it
 
 # Complete
-echo "\n \e[32m Deploy script complete."
+echo "\n ✅  Deploy script complete."
