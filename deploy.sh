5a1870dc5c1d2043b8ed8454609c52ace2776c76
Nate Dunn <hello@natedunn.net>
2019-03-28 12:08:37 -0600

    Try to fix deploy messages and remove gh-pages push

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,14 @@
 # Update Browser List data
-echo -e "\n\e[32mUpdating Browser List. Opening a browser window to continue."
+echo "\n \e[32m Updating Browser List. Opening a browser window to continue."
 npx browserslist-ga
 git add .
 git commit -m "Update Browser List"
 
 # Create release for Github
-echo -e "\n\e[32mReleasing a new version."
+echo "\n \e[32m Releasing a new version."
 source .env
 export GITHUB_TOKEN
 release-it
 
-# Deploy to gh-pages
-echo -e "\n\e[32mDeploying 'docs' directory to 'gh-pages' branch."
-git push origin `git subtree split --prefix docs master`:gh-pages --force
-
 # Complete
-echo -e "\n\e[32mDeploy script complete."
+echo "\n \e[32m Deploy script complete."
