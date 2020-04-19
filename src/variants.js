33dc4b1ea23bc412ee009ba7aa4a1c6facdb9d57
Nate Dunn <hello@natedunn.net>
2020-04-19 15:51:07 -0500

    Add important variant to display classes

--- src/variants.js
+++ src/variants.js
@@ -3,6 +3,7 @@ module.exports = {
   borderRadius: ['responsive', 'important'],
   textColor: ['responsive', 'hover', 'focus', 'important'],
   opacity: ['responsive', 'hover'],
+  display: ['responsive', 'important'],
   // Transition
   transitionProperty: ['responsive'],
   transitionDuration: ['responsive'],
