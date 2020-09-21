966a1685306ef3e587d0255ab2960496b8871d1f
Nate Dunn <hello@natedunn.net>
2020-09-21 16:24:32 -0500

    Bring theme into config

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,8 +2,89 @@ const plugins = require('./plugins');
 
 module.exports = {
   purge: false,
-  theme,
-  variants,
-  corePlugins: {},
+  theme: {
+    extend: {
+      colors: {
+        // Primary / 'Pewter'
+        primary: '#91B1A7',
+        'primary-light': '#b2c8c1',
+        'primary-dark': '#829e95',
+        // Secondary / 'Whale'
+        secondary: '#083549',
+        'secondary-light': '#52717f',
+        'secondary-dark': '#032636',
+        // Accent / 'Sunglow'
+        accent: '#FDD230',
+        'accent-light': '#fddf6e',
+        'accent-dark': '#e3bc2b',
+        // 'Baltic'
+        baltic: '#3E3E3F',
+        'baltic-light': '#616161',
+        'baltic-dark': '#313132',
+        // Seashell
+        seashell: '#E6E6E6',
+        'seashell-light': '#f0f0f0',
+        'seashell-dark': '#cecece',
+        // Other (grays)
+        whitesmoke: '#f5f5f5',
+        alice: '#f7f9fc',
+      },
+    },
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    fontFamily: {
+      sans: [
+        'Lato',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Helvetica',
+        'Arial',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+      ],
+      serif: [
+        'PT Serif',
+        'Constantia',
+        'Lucida Bright',
+        'Lucidabright',
+        'Lucida Serif',
+        'Lucida',
+        'DejaVu Serif',
+        'Bitstream Vera Serif',
+        'Liberation Serif',
+        'Georgia',
+        'serif',
+      ],
+      mono: [
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        'Liberation Mono',
+        'Courier New',
+        'monospace',
+      ],
+    },
+    inset: {
+      '0': '0',
+      '1': '1rem',
+      '2': '2rem',
+      '3': '3rem',
+      auto: 'auto',
+    },
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'important'],
+    borderRadius: ['responsive', 'important'],
+    textColor: ['responsive', 'hover', 'focus', 'important'],
+    opacity: ['responsive', 'hover'],
+    display: ['responsive', 'important'],
+  },
   plugins,
 };
