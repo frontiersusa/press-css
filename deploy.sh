37b39879d42bae8b31e6f2fa3b65c0d672c80618
Nate Dunn <hello@natedunn.net>
2020-09-21 13:25:41 -0500

    Remove old deploy code

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,3 @@
-# Update Browser List data
-# echo "\n 🌎  Updating Browser List. Opening a browser window to continue."
-# npx browserslist-ga
-# git add .
-# git commit -m "Update Browser List"
-
 # Create release for Github
 echo "\n 🚀  Releasing a new version."
 source .env
