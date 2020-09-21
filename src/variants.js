5af4da21f37353a9ce45ded28e54933103c77560
Nate Dunn <hello@natedunn.net>
2020-09-21 16:22:54 -0500

    Remove transition variants

--- src/variants.js
+++ src/variants.js
@@ -4,10 +4,4 @@ module.exports = {
   textColor: ['responsive', 'hover', 'focus', 'important'],
   opacity: ['responsive', 'hover'],
   display: ['responsive', 'important'],
-  // Transition
-  transitionProperty: ['responsive'],
-  transitionDuration: ['responsive'],
-  transitionTimingFunction: ['responsive'],
-  transitionDelay: ['responsive'],
-  willChange: ['responsive'],
 };
