f4329017ce13b8e53d87ac616c1c9d4da7e7e1f4
Nate Dunn <hello@natedunn.net>
2020-09-23 00:56:51 -0500

    Allow custom tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -2,31 +2,38 @@ require('dotenv').config();
 const purgeConfig = require('./purge.config');
 const { env } = process;
 
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss')('./src/tailwind.config.js'),
-    // Do NOT run in Press CSS Repo
-    ...env.NODE_ENV === 'production' && !env.IS_PRESS_REPO
-      ? [require('@fullhuman/postcss-purgecss')(purgeConfig)]
-      : [],
-    require('postcss-preset-env')({
-      stage: 1,
-      features: {
-        'focus-within-pseudo-class': false
-      }
-    }),
-    require('postcss-nesting'),
-    require('postcss-pxtorem')({
-      rootValue: 16,
-      propList: ['*'],
-      replace: true,
-      mediaQuery: false,
-    }),
-    require('postcss-flexbugs-fixes'),
-    ...env.NODE_ENV === 'production'
-      ? [require('cssnano')]
-      : []
-  ],
-  map: { inline: false },
+require('dotenv').config();
+const purgeConfig = require('./purge.config');
+const { env } = process;
+
+module.exports = function (tailwindConfig) {
+  const twConfig = tailwindConfig ? tailwindConfig : './src/tailwind.config.js';
+  return {
+    plugins: [
+      require('postcss-import'),
+      require('tailwindcss')(twConfig),
+      // Do NOT run in Press CSS Repo
+      ...env.NODE_ENV === 'production' && !env.IS_PRESS_REPO
+        ? [require('@fullhuman/postcss-purgecss')(purgeConfig)]
+        : [],
+      require('postcss-preset-env')({
+        stage: 1,
+        features: {
+          'focus-within-pseudo-class': false
+        }
+      }),
+      require('postcss-nesting'),
+      require('postcss-pxtorem')({
+        rootValue: 16,
+        propList: ['*'],
+        replace: true,
+        mediaQuery: false,
+      }),
+      require('postcss-flexbugs-fixes'),
+      ...env.NODE_ENV === 'production'
+        ? [require('cssnano')]
+        : []
+    ],
+    map: { inline: false },
+  }
 };
