a96e9630808f588f97b859e32987fc0692806122
Nate Dunn <hello@natedunn.net>
2020-09-23 01:03:23 -0500

    Fix import/require bugs

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,8 @@ require('dotenv').config();
 const purgeConfig = require('./purge.config');
 const { env } = process;
 
-require('dotenv').config();
-const purgeConfig = require('./purge.config');
-const { env } = process;
-
 module.exports = function (tailwindConfig) {
-  const twConfig = tailwindConfig ? tailwindConfig : './src/tailwind.config.js';
+  const twConfig = !env.IS_PRESS_REPO ? tailwindConfig : './src/tailwind.config.js';
   return {
     plugins: [
       require('postcss-import'),
